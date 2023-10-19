1bec82958b61158d0c9ed95e9cdabb157ccf7789
Javal Joseph <javal.joseph18@gmail.com>
2023-10-19 02:36:21 -0400

    Second Commit (Reminder Toggle)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Feed the Dogs',
+      date: 'Oct 25, 2023 at 7:00 AM',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Get Rich',
+      date: 'Dec 02, 2023 at 1:00 AM',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Die Trying',
+      date: 'Dec 02, 2024 at 1:00 AM',
+      reminder: true,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks Added'
+      )}
     </div>
   );
 };
